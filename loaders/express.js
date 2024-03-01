f085add6e33e8fd4aa1f4288276e655630d6891f
Ulashyn <ulashyn.o@gmail.com>
2024-03-01 11:30:18 +0200

    Revert "init app and add base structure"
    This reverts commit 33c0d922fb91062911f080d655e993b0f99a065a.

--- loaders/express.js
+++
@@ -1,9 +0,0 @@
-const expressLoader = async({app}) => {
-  // set main route
-  app.get('/', (req, res, next) => {
-    return res.send('Hayoken!');
-  });
-  return app;
-}
-
-module.exports = expressLoader;
--- loaders/index.js
+++
@@ -1,9 +0,0 @@
-const expressLoader = require('./express');
-
-const initLoader = async({expressApp}) => {
-  const app = await expressLoader({app: expressApp});
-  console.log('Express initialized');
-  return app;
-}
-
-module.exports = initLoader;
